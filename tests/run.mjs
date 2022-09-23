a0f65e9c47ae25ee82abe3efe6bd1ddcf0a7ea59
Mike Dial <michael.dial.4@gmail.com>
2022-09-23 09:05:44 -0400

    set up testing each bucket

--- tests/run.mjs
+++ tests/run.mjs
@@ -3,11 +3,13 @@ import LabeledProcessRunner from "./runner.mjs";
 import {
   CloudFormationClient,
   paginateDescribeStacks,
+  paginateListStackResources,
 } from "@aws-sdk/client-cloudformation";
 import _ from "lodash";
 const runner = new LabeledProcessRunner();
 const destroyer = new ServerlessStageDestroyer();
 const region = process.env.REGION_A;
+const project = process.env.PROJECT;
 
 Array.prototype.diff = function (arr2) {
   return this.filter((x) => !arr2.includes(x));
@@ -25,23 +27,61 @@ async function getAllStacksForRegion(region) {
 async function getAllStacksForStage(region, stage) {
   return (await getAllStacksForRegion(region))
     .filter((i) => i.Tags?.find((j) => j.Key == "STAGE" && j.Value == stage))
+    .filter((i) => i.Tags?.find((j) => j.Key == "PROJECT" && j.Value == project))
     .map((z) => z.StackName);
 }
 
+async function getBucketsForStack(region, stack) {
+  const client = new CloudFormationClient({ region: region });
+  const buckets = [];
+  for await (const page of paginateListStackResources(
+    { client },
+    { StackName: stack }
+  )) {
+    // The spread operator was causing an error, so using a for loop
+    for (let i of page.StackResourceSummaries || []) {
+      if (i.ResourceType == "AWS::S3::Bucket") {
+        buckets.push(i.PhysicalResourceId);
+      }
+    }
+  }
+  return buckets;
+}
+
 try {
-  await runner.run_command_and_output(
-    `deploy services`,
-    ["sls", "deploy", "--stage", process.env.STAGE_NAME],
-    "tests"
-  );
+  // await runner.run_command_and_output(
+  //   `deploy services`,
+  //   ["sls", "deploy", "--stage", process.env.STAGE_NAME],
+  //   "tests"
+  // );
+
+  // Iterate over each stack created for this project and stage, although there should only be one.
+  let stacks = await getAllStacksForStage(region, process.env.STAGE_NAME);
+  for(var i=0; i<stacks.length; i++) {
+    
+    // Iterate over each bucket created for this stack.
+    let buckets = await getBucketsForStack(region, stacks[i]);
+    for(var j=0; j<buckets.length; j++){
+
+      console.log(`Testing bucket: ${buckets[j]}`);
+      await testBucket(region, buckets[i]);
+      
+    }
+  }
+
 } catch (error) {
   throw error;
 } finally {
-  await destroyer.destroy(region, process.env.STAGE_NAME, {
-    verify: false,
-    wait: true,
-  });
+  // await destroyer.destroy(region, process.env.STAGE_NAME, {
+  //   verify: false,
+  //   wait: true,
+  // });
 }
+
+async function testBucket(region, bucket){
+  console.log(region + bucket);
+}
+
 
 // // ------------------------------------------------
 // console.log("\n\nChecking prod safeguard...");
@@ -57,125 +97,3 @@ try {
 // console.log("Check passed...");
 // // ------------------------------------------------
 
-// // ------------------------------------------------
-// console.log("\n\nChecking ability to destroy a stage.project.service...");
-// let before = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// await destroyer.destroy(region, process.env.STAGE_NAME, {
-//   filters: [
-//     {
-//       Key: "PROJECT",
-//       Value: "serverless-stage-destroyer",
-//     },
-//     {
-//       Key: "SERVICE",
-//       Value: "alpha",
-//     },
-//   ],
-//   verify: false,
-// });
-// let after = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// if (
-//   _.isEqual(
-//     before.diff(after).sort(),
-//     [`alpha-${process.env.STAGE_NAME}`].sort()
-//   )
-// ) {
-//   console.log("Check passed...");
-// } else {
-//   throw "ERROR:  Destruction of stage.project.service check failed.";
-// }
-// // ------------------------------------------------
-
-// // ------------------------------------------------
-// console.log("\n\nChecking ability to destroy a stage.project...");
-// before = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// await destroyer.destroy(region, process.env.STAGE_NAME, {
-//   filters: [
-//     {
-//       Key: "PROJECT",
-//       Value: "serverless-stage-destroyer",
-//     },
-//   ],
-//   verify: false,
-// });
-// after = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// if (
-//   _.isEqual(
-//     before.diff(after).sort(),
-//     [
-//       `bravo-${process.env.STAGE_NAME}`,
-//       `charlie-${process.env.STAGE_NAME}`,
-//       `delta-${process.env.STAGE_NAME}`,
-//     ].sort()
-//   )
-// ) {
-//   console.log("Check passed...");
-// } else {
-//   throw "ERROR:  Destruction of stage.project check failed.";
-// }
-// // ------------------------------------------------
-
-// // ------------------------------------------------
-// console.log("\n\nChecking ability to destroy a stage.service...");
-// before = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// await destroyer.destroy(region, process.env.STAGE_NAME, {
-//   filters: [
-//     {
-//       Key: "SERVICE",
-//       Value: "echo",
-//     },
-//   ],
-//   verify: false,
-// });
-// after = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// if (
-//   !_.isEqual(
-//     before.diff(after).sort(),
-//     [`echo-${process.env.STAGE_NAME}`].sort()
-//   )
-// ) {
-//   throw "ERROR:  Destruction of stage.service check failed.";
-// }
-// // ------------------------------------------------
-
-// // ------------------------------------------------
-// console.log("\n\nChecking ability to destroy a stage...");
-// before = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// await destroyer.destroy(region, process.env.STAGE_NAME, {
-//   verify: false,
-// });
-// after = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// if (
-//   !_.isEqual(
-//     before.diff(after).sort(),
-//     [`foxtrot-${process.env.STAGE_NAME}`].sort()
-//   )
-// ) {
-//   throw "ERROR:  Destruction of stage check failed.";
-// }
-// // ------------------------------------------------
-
-// // ------------------------------------------------
-// await deployAll();
-// // ------------------------------------------------
-
-// // ------------------------------------------------
-// console.log("\n\nChecking wait flag...");
-// before = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// await destroyer.destroy(region, process.env.STAGE_NAME, {
-//   verify: false,
-//   wait: false,
-// });
-// after = await getAllStacksForStage(region, process.env.STAGE_NAME);
-// if (after.length === 0) {
-//   throw "ERROR:  wait flag failed to work as intended...";
-// }
-// // ------------------------------------------------
-
-// console.log("Checks passed.  Cleaning up before exiting...");
-// while (
-//   (await getAllStacksForStage(region, process.env.STAGE_NAME)).length !== 0
-// ) {
-//   console.log("...");
-//   await new Promise((r) => setTimeout(r, 10000));
-// }
